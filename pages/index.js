63fcdd360e5b5982b6933b349138722cf0986b33
Roisin-13 <roisin.wheeler@gmail.com>
2021-11-22 18:30:03 +0000

    added static data as md file, read with remark

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import { getSortedPostsData } from '../lib/posts'
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 
-export default function Home() {
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,7 +18,31 @@ export default function Home() {
         </p>
       </section>
 
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
 
